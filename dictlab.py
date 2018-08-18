13a207083bf6dd6f54fc286ff975074319371efa
Senbagaraman04 <senbagaraman04@gmail.com>
2018-08-18 15:40:24 +0530

    dict lab

---
+++ dictlab.py
@@ -0,0 +1,26 @@
+#dict is a key valu pair data structure 
+
+Dict = { 'key1':'value1','key2':'value2' }
+
+print Dict
+
+print "Second key"
+
+print Dict['key2']
+
+soundtrack_dic = { "The Bodyguard":"1992", "Saturday Night Fever":"1977"}
+print soundtrack_dic 
+
+print soundtrack_dic.keys()
+print soundtrack_dic.values()
+
+album_sales_dict = {'Back in Black':'50', 'The Bodyguard' : '50','Thriller':'65'}
+print ("#print album_sales_dict['Thriller']")
+print album_sales_dict['Thriller']
+print ("#print album_sales_dict.keys()")
+print album_sales_dict.keys()
+print ("#print album_sales_dict.values()")
+print album_sales_dict.values()
+
+d = { 'a':1,'a':2}
+print d

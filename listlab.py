409eac4018895b23e3f1ead2b8663571d757c689
Senbagaraman04 <senbagaraman04@gmail.com>
2018-08-18 13:27:54 +0530

    List Lab data

---
+++ listlab.py
@@ -0,0 +1,23 @@
+print ("\n\nAuthor: Senbagaraman")
+print ("Lines printed with # are actual coding line, which will provide the desired result beneath it")
+print ("*************************************") 
+
+L=["Michael Jackson" , 10.1,1982]
+#print('the same element using negative and positive indexing:\n Postive:',L[0],'\n Negative:' , L[-3]  )
+#print('the same element using negative and positive indexing:\n Postive:',L[1],'\n Negative:' , L[-2]  )
+#print('the same element using negative and positive indexing:\n Postive:',L[2],'\n Negative:' , L[-1]  )
+
+j = 'hard rock'.split()
+
+print (j)
+#a_list = [1,"hello",[1,2,3],true]
+#print (a_list)
+B =L [:]
+print B
+
+L.append(["test"])
+
+print L
+
+x = len(("disco",10))
+print (x)

088215cf3dc3c01f6f14eac23b1572cc8a51ea55
Senbagaraman04 <senbagaraman04@gmail.com>
2018-09-22 13:09:14 +0530

    Remaining Left files

---
+++ class.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot
+
+
+class Circle(object):
+    
+    def __init__(self,radius=3,color='blue'):
+        
+        self.radius=radius
+        self.color=color 
+    
+    def add_radius(self,r):
+        
+        self.radius=self.radius+r
+        return(self.radius)
+    def drawCircle(self):
+        
+        plt.gca().add_patch(plt.Circle((0, 0), radius=self.radius, fc=self.color))
+        plt.axis('scaled')
+        plt.show()  
+
+RedCircle=Circle(10,'red')
+dir(RedCircle)
+
+RedCircle.drawCircle()
---
+++ ex.py
@@ -0,0 +1,17 @@
+for i,x in enumerate(['A','B','C']):
+    print(i,2*x)
+
+for i in range(1,5):
+    if (i!=2):
+        print(i)
+
+
+def add(x):
+    return(x+x)
+    
+print (add('1'))
+
+v  = [ 'a','w','b']
+v.sort()
+
+print (v)
